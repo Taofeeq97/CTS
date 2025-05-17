4b3a195769d0b5e2092db58c502e82d0b1472569
Taofeeq97 <otutaofeeqi@gmail.com>
2025-05-17 10:50:22 +0100

    fix

--- agri/views.py
+++ agri/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, status, filters
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView 
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Farmer, CollectionCenter, ProcessingFacility, PackagingCenter, Batch
 from .serializers import (
